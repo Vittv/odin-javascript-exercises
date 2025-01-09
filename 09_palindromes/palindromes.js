cc6806d7aeb4da8a0b1aa808c2c562f30af19771
Vittv <166668150+Vittv@users.noreply.github.com>
2025-01-09 18:58:43 -0300

    Resolve exercises with all tests passing

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // Clean the string, making it all lowercase and free of punctuation
+    const cleanedString = string
+        .toLowerCase()
+        .split("")
+        .filter(char => (char >= "a" && char <= "z") || (char >= "0" && char <= "9"))
+        .join("")
 
+    // Reverse it
+    const invertedString = cleanedString.split("").reverse().join("");
+
+    // Compare both
+    return cleanedString === invertedString
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(givenNumber) {
+    // Convert the input to a number
+    givenNumber = Number(givenNumber);
 
+    // Handle edge cases
+    if (givenNumber === 0) return 0;
+    if (givenNumber === 1) return 1;
+    if (givenNumber < 0) return "OOPS";
+
+    // Track the sequence
+    let previous = 0, current = 1;
+
+    Array(givenNumber - 1).fill(0).forEach(() => {
+        let next = previous + current;
+        previous = current; // Update previous
+        current = next;     // Update current
+    });
+
+    return current; // Return the final number
 };
 
 // Do not edit below this line
